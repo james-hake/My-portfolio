453593414a971eb9b0faa66ed1af33948cd45b8d
James Hake <161508699+james-hake@users.noreply.github.com>
2024-03-03 23:02:41 +0000

    fixed extenstion error

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,7 +19,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/tablet.jpb";
+import image from "../images/tablet.jpg";
 
 const imageAltText = "A man holding tablet.";
 
