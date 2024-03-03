ff7e7ef3f0414c1b9422d39373800c4b77874df9
James Hake <161508699+james-hake@users.noreply.github.com>
2024-03-03 23:00:00 +0000

    added my github details

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "James hake",
+  title: "Cybersecurity Engineer, Web Designer & Content Creator",
+  email: "hake.james@outlook.com",
+  gitHub: "https://github.com/james-hake",
+  instagram: "",
+  linkedIn: "linkedin.com/in/jameshake",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "https://twitter.com/james_hake",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
